1c1dfcf49f987c37c7e3ecf62e1ca88b5a2b271d
AyoubZekri <ayobzekri670@email.com>
2026-04-18 16:13:24 +0100

    04/18/2025 2:55

--- app/Http/Controllers/AppLication/Institutions.php
+++ app/Http/Controllers/AppLication/Institutions.php
@@ -30,29 +30,29 @@ class Institutions extends Controller
         $data = Institution::query()
             ->where('scope', $request->scope)
             ->orderBy('index', 'asc')
-            // ->with('laws.law') // بدون تعقيد
-            // ->with([
-            //     'reads' => function($q) use ($userId) {
-            //         $q->where('user_id', $userId);
-            //     }
-            // ])
-            ->get();
-            // ->map(function($item) {
-            //     $item->is_read = $item->reads->count() > 0;
-            //     unset($item->reads);
+            ->with('laws.law') // بدون تعقيد
+            ->with([
+                'reads' => function($q) use ($userId) {
+                    $q->where('user_id', $userId);
+                }
+            ])
+            ->get()
+            ->map(function($item) {
+                $item->is_read = $item->reads->count() > 0;
+                unset($item->reads);
 
-            //     $item->laws = $item->laws->map(function ($law) {
-            //         return [
-            //             'law_id'     => $law->law_id,
-            //             'name_ar'    => $law->name_ar,
-            //             'name_fr'    => $law->name_fr,
-            //             'index_link' => $law->index_link,
-            //             'pdf'        => optional($law->law)->pdf,
-            //         ];
-            //     });
+                $item->laws = $item->laws->map(function ($law) {
+                    return [
+                        'law_id'     => $law->law_id,
+                        'name_ar'    => $law->name_ar,
+                        'name_fr'    => $law->name_fr,
+                        'index_link' => $law->index_link,
+                        'pdf'        => optional($law->law)->pdf,
+                    ];
+                });
 
-            //     return $item;
-            // });
+                return $item;
+            });
             return Respons::success(
                  $data
             );
