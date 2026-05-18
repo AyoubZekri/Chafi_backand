3b79ffadfad277f8df92f5d09881465222468026
Zineb-web22 <bzineb296@gmail.com>
2026-05-18 17:52:13 +0100

    2026/05/18 17:00

--- app/Models/Categories_cat_insts.php
+++ app/Models/Categories_cat_insts.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Categories_cat_insts extends Model
 {
     use HasFactory;
-
+    protected $table = "categories_cat_insts";
     protected $fillable = ['index', 'name', 'name_fr', "cat_id"];
 
     public function Categories_institutions()
