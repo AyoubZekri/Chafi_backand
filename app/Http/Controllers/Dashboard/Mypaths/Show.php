6d1aec57e5d13691bf6565bcb2a1c4ae01741a7f
Zineb-web22 <bzineb296@gmail.com>
2026-05-19 13:49:18 +0100

    2026/05/19 12:00

--- app/Http/Controllers/Dashboard/Mypaths/Show.php
+++ app/Http/Controllers/Dashboard/Mypaths/Show.php
@@ -11,59 +11,59 @@ use Illuminate\Support\Facades\Validator;
 
 class Show extends Controller
 {
-public function show(Request $request)
-{
-    try {
-        $validator = Validator::make($request->all(), [
-            'id'=> 'nullable|integer',
-        ]);
+    public function show(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'nullable|integer',
+            ]);
 
-        if ($validator->fails()) {
-            return Respons::error(
-                'بيانات غير صحيحة',
-                422,
-                $validator->errors()
-            );
+            if ($validator->fails()) {
+                return Respons::error(
+                    'بيانات غير صحيحة',
+                    422,
+                    $validator->errors()
+                );
+            }
+
+            $query = Mypath::query()
+                ->with([
+                    'user:id,username,wilaya',
+                    'activity:id,name,name_fr,code_activity',
+                    'nataire_activitys:id,name,name_fr'
+                ])
+                ->where('user_id', auth()->id());
+
+            if ($request->filled('id')) {
+                $query->where('id', $request->id);
+            }
+
+            $data = $query->get()->map(function ($item) {
+                // بيانات mypaths الأصلية
+                $mypathData = $item->toArray();
+
+                // إضافة الأعمدة من العلاقات مباشرة
+                $mypathData['username'] = $item->user?->username;
+                $mypathData['wilaya'] = $item->user?->wilaya;
+                $mypathData['activity_name'] = $item->activity?->body;
+                $mypathData['activity_name_fr'] = $item->activity?->body_fr;
+                $mypathData['code_activity'] = $item->activity?->code_activity;
+                $mypathData['activity_tax_id'] = $item->activity?->tax_id;
+                $mypathData['nataire_activitys_name'] = $item->nataire_activitys?->name;
+                $mypathData['nataire_activitys_name_fr'] = $item->nataire_activitys?->name_fr;
+
+                // حذف أي JSON nested
+                unset($mypathData['user'], $mypathData['activity'], $mypathData['nataire_activitys']);
+
+                return $mypathData;
+            });
+
+            return Respons::success($data);
+
+        } catch (\Exception $e) {
+            return Respons::error('غير موجودة', 404);
         }
-
-        $query = Mypath::query()
-            ->with([
-                'user:id,username,wilaya',
-                'activity:id,name,name_fr,code_activity',
-                'nataire_activitys:id,name,name_fr'
-            ])
-            ->where('user_id', auth()->id());
-
-        if ($request->filled('id')) {
-            $query->where('id', $request->id);
-        }
-
-        $data = $query->get()->map(function($item) {
-            // بيانات mypaths الأصلية
-            $mypathData = $item->toArray();
-
-            // إضافة الأعمدة من العلاقات مباشرة
-            $mypathData['username'] = $item->user?->username;
-            $mypathData['wilaya'] = $item->user?->wilaya;
-            $mypathData['activity_name'] = $item->activity?->name;
-            $mypathData['activity_name_fr'] = $item->activity?->name_fr;
-            $mypathData['code_activity'] = $item->activity?->code_activity;
-            $mypathData['activity_tax_id'] = $item->activity?->tax_id;
-            $mypathData['nataire_activitys_name'] = $item->nataire_activitys?->name;
-            $mypathData['nataire_activitys_name_fr'] = $item->nataire_activitys?->name_fr;
-
-            // حذف أي JSON nested
-            unset($mypathData['user'], $mypathData['activity'], $mypathData['nataire_activitys']);
-
-            return $mypathData;
-        });
-
-        return Respons::success($data);
-
-    } catch (\Exception $e) {
-        return Respons::error('غير موجودة', 404);
     }
-}
 
 
 }
