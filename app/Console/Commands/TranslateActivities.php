0fa464710c882ea823fb835c86c3eff18bcac114
Zineb-web22 <bzineb296@gmail.com>
2026-05-23 10:48:13 +0100

    2026/05/23 1:21

--- app/Console/Commands/TranslateActivities.php
+++ app/Console/Commands/TranslateActivities.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\Categories_cat_insts;
+use App\Models\Activity;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class TranslateActivities extends Command
@@ -20,27 +20,25 @@ class TranslateActivities extends Command
 
         $count = 0;
 
-        Categories_cat_insts::chunkById(50, function ($activities) use ($translator, &$count) {
+        Activity::chunk(50, function ($activities) use ($translator, &$count) {
 
             foreach ($activities as $activity) {
 
                 try {
 
-                    $data = [];
-
-                    if (filled($activity->name) && !filled($activity->name_fr)) {
-                        $data['name_fr'] = $translator->translate($activity->name);
+                    if (!empty($activity->name) && empty($activity->name_fr)) {
+                        $activity->name_fr = $translator->translate($activity->name);
                     }
 
-                    // if (filled($activity->body) && !filled($activity->body_fr)) {
-                    //     $data['body_fr'] = $translator->translate($activity->body);
-                    // }
-
-                    if (!empty($data)) {
-                        $activity->update($data);
-                        $count++;
-                        $this->info("Translated ID: {$activity->id}");
+                    if (!empty($activity->body) && empty($activity->body_fr)) {
+                        $activity->body_fr = $translator->translate($activity->body);
+                        $this->info("Translated body ID: {$activity->body_fr}");
                     }
+
+                    $activity->save();
+                    $count++;
+
+                    $this->info("Translated ID: {$activity->id}");
 
                 } catch (\Exception $e) {
                     $this->error("Error ID {$activity->id}: " . $e->getMessage());
@@ -49,5 +47,7 @@ class TranslateActivities extends Command
         });
 
         $this->info("DONE. Total translated: {$count}");
+
+        return 0;
     }
 }
