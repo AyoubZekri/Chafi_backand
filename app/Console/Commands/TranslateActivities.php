860d78073280e1048aefebcfddd113d41fd0c783
Zineb-web22 <bzineb296@gmail.com>
2026-05-23 11:30:15 +0100

    2026/05/23 2:21

--- app/Console/Commands/TranslateActivities.php
+++ app/Console/Commands/TranslateActivities.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Bonusesandcompensations;
 use App\Models\Categories_cat_insts;
+use App\Models\Categories_differents;
 use Illuminate\Console\Command;
 use App\Models\Activity;
 use Stichoza\GoogleTranslate\GoogleTranslate;
@@ -22,7 +23,7 @@ class TranslateActivities extends Command
 
         $count = 0;
 
-        Categories_cat_insts::chunk(50, function ($activities) use ($translator, &$count) {
+        Categories_differents::chunk(50, function ($activities) use ($translator, &$count) {
 
             foreach ($activities as $activity) {
 
