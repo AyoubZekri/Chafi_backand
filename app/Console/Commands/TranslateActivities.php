7de20e902a2b7ed8c82aa5e506ff19e98003d7e4
Zineb-web22 <bzineb296@gmail.com>
2026-05-23 11:55:28 +0100

    2026/05/23 2:21

--- app/Console/Commands/TranslateActivities.php
+++ app/Console/Commands/TranslateActivities.php
@@ -10,6 +10,7 @@ use App\Models\Category;
 use App\Models\Different;
 use App\Models\DifferentLaw;
 use App\Models\Institution;
+use App\Models\InstitutionLaw;
 use Illuminate\Console\Command;
 use App\Models\Activity;
 use Stichoza\GoogleTranslate\GoogleTranslate;
@@ -28,21 +29,21 @@ class TranslateActivities extends Command
 
         $count = 0;
 
-        Institution::chunk(50, function ($activities) use ($translator, &$count) {
+        InstitutionLaw::chunk(50, function ($activities) use ($translator, &$count) {
 
             foreach ($activities as $activity) {
 
                 try {
 
-                    // $activity->name_fr = $translator->translate($activity->name_ar);
-                    // $this->info("Translated name ID: {$activity->name_fr}");
+                    $activity->name_fr = $translator->translate($activity->name_ar);
+                    $this->info("Translated name ID: {$activity->name_fr}");
 
-                    $activity->title_fr = $translator->translate($activity->title);
-                    $this->info("Translated title ID: {$activity->title_fr}");
-                    if (!empty($activity->body)) {
-                        $activity->body_fr = $translator->translate($activity->body);
-                        $this->info("Translated body ID: {$activity->body_fr}");
-                    }
+                    // $activity->title_fr = $translator->translate($activity->title);
+                    // $this->info("Translated title ID: {$activity->title_fr}");
+                    // if (!empty($activity->body)) {
+                    //     $activity->body_fr = $translator->translate($activity->body);
+                    //     $this->info("Translated body ID: {$activity->body_fr}");
+                    // }
 
                     $activity->save();
                     $count++;
